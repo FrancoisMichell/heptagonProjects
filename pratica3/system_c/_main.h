b4f2de8c4af42093a02da4d7e195851668c3633e
FMichell <francois_michell95@hotmail.com>
2018-06-06 23:14:31 -0300

    finished p3

--- pratica3/system_c/_main.h
+++
@@ -1,9 +0,0 @@
-/* --- Generated the 22/5/2018 at 22:25 --- */
-/* --- heptagon compiler, version 1.03.00 (compiled fri. jan. 19 10:22:24 CET 2018) --- */
-/* --- Command line: /usr/local/bin/heptc -target c -target z3z -s controller system.ept --- */
-
-#ifndef _MAIN_H
-#define _MAIN_H
-
-#include "system.h"
-#endif // _MAIN_H
